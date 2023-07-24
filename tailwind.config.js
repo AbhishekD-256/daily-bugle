3361408414369e40dc491c0375863e47dbda0269
Abhishek Dolai <itsabhi183@gmail.com>
2023-07-24 15:44:52 +0530

    add responsive breakpoins and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      lg: "1300px",
+    },
     colors: {
       transparent: "transparent",
       primary: "#D73939",
