9be65aa08c4e04e5bf6f5be1bceb93d1233e40f0
Abhishek Dolai <itsabhi183@gmail.com>
2023-07-25 17:58:18 +0530

    add headlines data dynamically and make see all functionality

--- dist/main.js
+++ dist/main.js
@@ -24,9 +24,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sass_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sass/main.scss */ \"./src/sass/main.scss\");\n\r\n\n\n//# sourceURL=webpack://project-k-setup/./src/index.js?");
+eval("__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {\n__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sass_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sass/main.scss */ \"./src/sass/main.scss\");\n\r\n\r\nconst headlinesContainer = document.getElementById(\"todays-headlines\");\r\nconst seeAllBtn = document.getElementById(\"see-all\");\r\n\r\nconst API =\r\n  \"https://api.nytimes.com/svc/topstories/v2/home.json?api-key=bAyMmUuJ8Aa2hOxXViAcZBdgKjrIenYU\";\r\n\r\nlet results;\r\n\r\nfunction renderHeadlines(results, index) {\r\n  headlinesContainer.innerHTML = \"\";\r\n  for (let i = 0; i <= index; i++) {\r\n    const result = results[i];\r\n    const headlineHtml = `\r\n        <li class=\"w-336  flex  gap-sm\">\r\n            <figure class=\"w-[102px]  h-[96px]  rounded-sm  overflow-hidden  shrink-0  object-cover\">\r\n                <img class=\"h-full w-full\"\r\n                    src=\"${result.multimedia[1].url}\"\r\n                    alt=\"king pin knocked down\">\r\n            </figure>\r\n            <div class=\"flex  flex-col justify-center  gap-xsm    grow\">\r\n                <h3 class=\"font-bold  truncate  max-w-[200px]  text-md\">${result.title}\r\n                </h3>\r\n                <p class=\"truncate-3  max-w-[200px]  text-sm\">${result.abstract}</p>\r\n            </div>\r\n       </li>\r\n        `;\r\n    headlinesContainer.insertAdjacentHTML(\"beforeend\", headlineHtml);\r\n  }\r\n}\r\n\r\nasync function getData() {\r\n  const response = await fetch(API);\r\n  const data = await response.json();\r\n  results = data.results;\r\n  console.log(results);\r\n  renderHeadlines(results, 7);\r\n}\r\nawait getData();\r\n\r\nlet all = false;\r\n\r\nseeAllBtn.addEventListener(\"click\", function () {\r\n  if (all) {\r\n    renderHeadlines(results, 7);\r\n    all = false;\r\n  } else if (!all) {\r\n    renderHeadlines(results, results.length - 1);\r\n    all = true;\r\n  }\r\n});\r\n\n__webpack_async_result__();\n} catch(e) { __webpack_async_result__(e); } }, 1);\n\n//# sourceURL=webpack://project-k-setup/./src/index.js?");
 
 /***/ })
 
@@ -57,6 +57,75 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sas
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/async module */
+/******/ 	(() => {
+/******/ 		var webpackQueues = typeof Symbol === "function" ? Symbol("webpack queues") : "__webpack_queues__";
+/******/ 		var webpackExports = typeof Symbol === "function" ? Symbol("webpack exports") : "__webpack_exports__";
+/******/ 		var webpackError = typeof Symbol === "function" ? Symbol("webpack error") : "__webpack_error__";
+/******/ 		var resolveQueue = (queue) => {
+/******/ 			if(queue && queue.d < 1) {
+/******/ 				queue.d = 1;
+/******/ 				queue.forEach((fn) => (fn.r--));
+/******/ 				queue.forEach((fn) => (fn.r-- ? fn.r++ : fn()));
+/******/ 			}
+/******/ 		}
+/******/ 		var wrapDeps = (deps) => (deps.map((dep) => {
+/******/ 			if(dep !== null && typeof dep === "object") {
+/******/ 				if(dep[webpackQueues]) return dep;
+/******/ 				if(dep.then) {
+/******/ 					var queue = [];
+/******/ 					queue.d = 0;
+/******/ 					dep.then((r) => {
+/******/ 						obj[webpackExports] = r;
+/******/ 						resolveQueue(queue);
+/******/ 					}, (e) => {
+/******/ 						obj[webpackError] = e;
+/******/ 						resolveQueue(queue);
+/******/ 					});
+/******/ 					var obj = {};
+/******/ 					obj[webpackQueues] = (fn) => (fn(queue));
+/******/ 					return obj;
+/******/ 				}
+/******/ 			}
+/******/ 			var ret = {};
+/******/ 			ret[webpackQueues] = x => {};
+/******/ 			ret[webpackExports] = dep;
+/******/ 			return ret;
+/******/ 		}));
+/******/ 		__webpack_require__.a = (module, body, hasAwait) => {
+/******/ 			var queue;
+/******/ 			hasAwait && ((queue = []).d = -1);
+/******/ 			var depQueues = new Set();
+/******/ 			var exports = module.exports;
+/******/ 			var currentDeps;
+/******/ 			var outerResolve;
+/******/ 			var reject;
+/******/ 			var promise = new Promise((resolve, rej) => {
+/******/ 				reject = rej;
+/******/ 				outerResolve = resolve;
+/******/ 			});
+/******/ 			promise[webpackExports] = exports;
+/******/ 			promise[webpackQueues] = (fn) => (queue && fn(queue), depQueues.forEach(fn), promise["catch"](x => {}));
+/******/ 			module.exports = promise;
+/******/ 			body((deps) => {
+/******/ 				currentDeps = wrapDeps(deps);
+/******/ 				var fn;
+/******/ 				var getResult = () => (currentDeps.map((d) => {
+/******/ 					if(d[webpackError]) throw d[webpackError];
+/******/ 					return d[webpackExports];
+/******/ 				}))
+/******/ 				var promise = new Promise((resolve) => {
+/******/ 					fn = () => (resolve(getResult));
+/******/ 					fn.r = 0;
+/******/ 					var fnQueue = (q) => (q !== queue && !depQueues.has(q) && (depQueues.add(q), q && !q.d && (fn.r++, q.push(fn))));
+/******/ 					currentDeps.map((dep) => (dep[webpackQueues](fnQueue)));
+/******/ 				});
+/******/ 				return fn.r ? promise : getResult();
+/******/ 			}, (err) => ((err ? reject(promise[webpackError] = err) : outerResolve(exports)), resolveQueue(queue)));
+/******/ 			queue && queue.d < 0 && (queue.d = 0);
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
 import "./sass/main.scss";
+
+const headlinesContainer = document.getElementById("todays-headlines");
+const seeAllBtn = document.getElementById("see-all");
+
+const API =
+  "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=bAyMmUuJ8Aa2hOxXViAcZBdgKjrIenYU";
+
+let results;
+
+function renderHeadlines(results, index) {
+  headlinesContainer.innerHTML = "";
+  for (let i = 0; i <= index; i++) {
+    const result = results[i];
+    const headlineHtml = `
+        <li class="w-336  flex  gap-sm">
+            <figure class="w-[102px]  h-[96px]  rounded-sm  overflow-hidden  shrink-0  object-cover">
+                <img class="h-full w-full"
+                    src="${result.multimedia[1].url}"
+                    alt="king pin knocked down">
+            </figure>
+            <div class="flex  flex-col justify-center  gap-xsm    grow">
+                <h3 class="font-bold  truncate  max-w-[200px]  text-md">${result.title}
+                </h3>
+                <p class="truncate-3  max-w-[200px]  text-sm">${result.abstract}</p>
+            </div>
+       </li>
+        `;
+    headlinesContainer.insertAdjacentHTML("beforeend", headlineHtml);
+  }
+}
+
+async function getData() {
+  const response = await fetch(API);
+  const data = await response.json();
+  results = data.results;
+  console.log(results);
+  renderHeadlines(results, 7);
+}
+await getData();
+
+let all = false;
+
+seeAllBtn.addEventListener("click", function () {
+  if (all) {
+    renderHeadlines(results, 7);
+    all = false;
+  } else if (!all) {
+    renderHeadlines(results, results.length - 1);
+    all = true;
+  }
+});
