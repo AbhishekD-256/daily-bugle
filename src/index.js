c2d2cc9828f0c1e72dcae43964058e54b724554d
Abhishek Dolai <itsabhi183@gmail.com>
2023-07-26 17:28:19 +0530

    add dropdown and change todays headlines dynamically

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import "./sass/main.scss";
 
 const headlinesContainer = document.getElementById("todays-headlines");
 const seeAllBtn = document.getElementById("see-all");
-
-const API =
-  "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=bAyMmUuJ8Aa2hOxXViAcZBdgKjrIenYU";
+const dropdownList = document.getElementById("dropdown-list");
+const curItemName = document.getElementById("selected-item");
 
 let results;
+let seeAll = false;
 
 function renderHeadlines(results, index) {
   headlinesContainer.innerHTML = "";
@@ -30,23 +30,35 @@ function renderHeadlines(results, index) {
   }
 }
 
-async function getData() {
-  const response = await fetch(API);
+async function getData(section = "home") {
+  seeAll = false;
+  const response = await fetch(
+    `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=bAyMmUuJ8Aa2hOxXViAcZBdgKjrIenYU`
+  );
   const data = await response.json();
   results = data.results;
-  console.log(results);
   renderHeadlines(results, 7);
 }
 getData();
 
-let all = false;
-
 seeAllBtn.addEventListener("click", function () {
-  if (all) {
+  if (seeAll) {
     renderHeadlines(results, 7);
-    all = false;
-  } else if (!all) {
+    seeAll = false;
+  } else if (!seeAll) {
     renderHeadlines(results, results.length - 1);
-    all = true;
+    seeAll = true;
+  }
+});
+
+document.addEventListener("click", function (e) {
+  const ele = e.target.closest("#js-dropdown");
+  if (!ele) dropdownList.classList.add("hidden");
+  else {
+    dropdownList.classList.toggle("hidden");
+    const listItem = e.target;
+    if (!listItem.classList.contains("dropdown-item")) return;
+    curItemName.textContent = listItem.textContent;
+    getData(listItem.textContent);
   }
 });
